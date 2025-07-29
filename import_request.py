27d66da46d076a5fb5c3baab8878dfd68f2aae4c
reomoon <taehyung-moon@nhnservice.com>
2025-07-29 16:40:25 +0900

    v1.01 주석 추가

--- import_request.py
+++ import_request.py
@@ -1,18 +1,20 @@
 import requests
 import time
 
+# 인천공항 제2터미널 주차장 예약 API를 사용하여
+# check?date 날짜를 변경하여 true false를 확인할 수 있습니다.
 url = "https://api.amanopark.co.kr/api/web/setting/booking/check?date=2025-08-01&type=BASIC"
 
-while True:
+while True: # true가 될 때까지 반복
     try:
         response = requests.get(url)
         if response.status_code == 200:
-            result = response.json()
+            result = response.json() # JSON 응답을 파싱
             if result.get("data") is True:
                 print("✅data가 True로 변경되었습니다!")
-                break
-            else:
-                print("❗아직 data가 True가 아닙니다.")
+                break # true가 되면 반복문 종료
+            else: # data가 False인 경우
+                print("❗data가 False 입니다.")
         else:
             print(f"HTTP 오류: {response.status_code}")
     except Exception as e:
